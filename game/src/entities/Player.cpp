e8a1cbdeb0c4f0c224ff0a3f2b97343e749eb8a7
sugham019 <sughamkharel123@gmail.com>
2024-12-05 18:54:57 +0545

    Update speed

--- game/src/entities/Player.cpp
+++ game/src/entities/Player.cpp
@@ -2,6 +2,6 @@
 #include <SFML/System/Vector2.hpp>
 
 Player::Player(sf::Sprite& sprite, const sf::Vector2f& spawnPosition, float healthPoints, float attackDamage, bool isRigid)
-    : Character(healthPoints, attackDamage, sprite, spawnPosition, isRigid, GameObjectType::PLAYER, 220.0f){
+    : Character(healthPoints, attackDamage, sprite, spawnPosition, isRigid, GameObjectType::PLAYER, 0.2f){
 
 }
